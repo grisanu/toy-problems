65bd4f2e1f5dc2256fb2a084bfa2fca3ab95bd36
Grisanu Naing <mgrisanu@gmail.com>
2016-09-27 16:20:40 -0700

    (feat) Add addChild and removeChild functions

--- BST/index.js
+++ BST/index.js
@@ -11,16 +11,55 @@ const BST = function (root) {
   this.right = null;
 };
 
-const Node = function (value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-};
-
 BST.prototype.addChild = function (value) {
-  // cannot add duplicate
+  if (this.value === value) {
+    // cannot add duplicate
+    return;
+  } else if (this.value < value) {
+    if (this.left === null) {
+      this.left = new BST(value);
+    } else {
+      this.left.addChild(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = new BST(value);
+    } else {
+      this.right.addChild(value);
+    }
+  }
 };
 
 BST.prototype.removeChild = function (value) {
+  if (this.value === value) {
+    return true;
+  } else if (this.value < value) {
+    if (this.left === null) {
+      return false;
+    } else if (this.left.removeChild(value)) {
+      this.left = null;
+    }
+  } else {
+    if (this.right === null) {
 
+    } else if (this.right.removeChild(value)){
+      this.right = null;
+    }
+  }
 };
+
+const aa = new BST(10);
+aa.addChild(6);
+aa.addChild(11);
+aa.addChild(3);
+aa.addChild(1);
+aa.addChild(20);
+aa.addChild(13);
+aa.addChild(16);
+aa.addChild(8);
+
+console.log(aa);
+
+aa.removeChild(2)
+
+console.log(aa);
